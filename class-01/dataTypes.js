65f0dc2debc767c82538be4a1bff6893566103ff
Adil Hussain <146125640+m-adil-hussain@users.noreply.github.com>
2024-08-25 10:05:31 +0000

    come changes for practice purpos

--- class-01/dataTypes.js
+++ class-01/dataTypes.js
@@ -7,3 +7,39 @@
         adil    
     "
 ); */ // code readability shound be high
+
+let name = "adil";
+let age = 20;
+let userLogedIn = true;
+let state = null;
+
+// PREMETIVE DATA TYPES:
+/*
+    Number => 2 to power 53
+    BigInt
+    String => ""
+    Boolean => TRUE / FALSE
+    Null => Standalone Value
+    Undefined
+    Symbol => Unique
+*/
+
+// NON PREMITIVE DATA TYPES:
+/*
+    Object
+
+*/
+
+console.table(
+    [
+        typeof age,
+        typeof name,
+        typeof userLogedIn,
+        typeof state,
+        typeof undefined,
+        typeof {},
+        typeof [],
+        typeof 9007199254740991n,
+        typeof Symbol()
+    ]
+);
