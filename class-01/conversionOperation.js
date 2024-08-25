d5a7416d3bf3a61d048605ce58de5b52b411fcc8
Adil Hussain <146125640+m-adil-hussain@users.noreply.github.com>
2024-08-25 10:27:07 +0000

    some changes for practice purpose

---
+++ class-01/conversionOperation.js
@@ -0,0 +1,23 @@
+let num = "55";
+
+console.log(typeof num);
+
+let valueInNumber = Number(num);
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+/*
+    "55" => number
+    "55ABC" => NAN
+
+*/
+
+let str = "55";
+
+console.log(typeof str);
+
+let valueInString = String(str);
+
+console.log(typeof valueInString);
+console.log(valueInString);
