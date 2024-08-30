976d8ff2a69954c61362b2a5fb3071fd8b613ad8
Adil Hussain <146125640+m-adil-hussain@users.noreply.github.com>
2024-08-30 19:04:56 +0000

    some changes for practice purpose

--- class-01/dataTypesSummary.js
+++ class-01/dataTypesSummary.js
@@ -11,6 +11,15 @@
     BigInt
 */
 
+let name = "Adil Hussain"; // String
+let age = 20; // Number
+let male = true; // Boolean
+let email = null; // Null
+let fullName; // Undefined
+let specialChar = Symbol("#"); // Symbol
+let ipAddress = 1119997772211n; // BigInt
+
+
 // NON PRIMITIVE (Refrence) DATA TYPES
 
 /*
@@ -18,3 +27,20 @@
     Objects
     Functions
 */
+
+
+let array = [
+    "Apple",
+    "Banana",
+    "Orange"
+]; // Array
+
+let Object = {
+    name: "Adil",
+    email: "adil@gmail.com",
+    phone: +923344556677
+} // Object
+
+let fun = function () {
+    console.log("adil Hussain");
+}();
