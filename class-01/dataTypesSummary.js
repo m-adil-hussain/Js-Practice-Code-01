4bf5be17dfda250ec1d8926964069ef20e2d16ad
Adil Hussain <146125640+m-adil-hussain@users.noreply.github.com>
2024-08-30 18:52:57 +0000

    some changes for practice purpose

---
+++ class-01/dataTypesSummary.js
@@ -0,0 +1,20 @@
+// PRIMITIVE DATA TYPES
+
+/*
+    7 TYPES:
+    String
+    Number
+    Boolean
+    Null
+    Undefined
+    Symbol
+    BigInt
+*/
+
+// NON PRIMITIVE (Refrence) DATA TYPES
+
+/*
+    Array
+    Objects
+    Functions
+*/
