2eec766c0a3a3ef0b596be3bcd7bef8e1ada29e8
Adil Hussain <146125640+m-adil-hussain@users.noreply.github.com>
2024-09-07 17:50:34 +0000

    some canges for practice purpose

--- class-02/array.js
+++ class-02/array.js
@@ -29,5 +29,25 @@ console.log(arrSplice);
 
 console.log(arr); // Old Array Mai Jo Yalue Reh Jati Hai Wo Return Krta Hai */
 
-arr.push(frtArr);
-console.log(arr[5][0]);
+// arr.push(frtArr);
+// console.log(arr); // ❌
+
+// const concatArr = arr.concat(frtArr);
+// console.log(concatArr); // ✅
+
+// const sprdOprt = [...arr, ...frtArr];
+// console.log(sprdOprt); // ✅
+
+const arrInArr = [1, 2, 3, 4, [0, 1, 2, [3, 4, 5, 6], 7, 8, 9], 5, 6, 7];
+const flatArr = arrInArr.flat(Infinity);
+console.log(flatArr);
+
+
+console.log(Array.isArray("Adil hussain")); // Question is Array??
+console.log(Array.from("Adil hussain")); // Convert Array
+
+const score1 = 100;
+const score2 = 200;
+const score3 = 300;
+
+console.log(Array.of(score1, score2, score3));
