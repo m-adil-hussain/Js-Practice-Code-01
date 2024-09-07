9ddfc236815478e2df5b48087efe4df2b49a16ad
Adil Hussain <146125640+m-adil-hussain@users.noreply.github.com>
2024-09-07 16:15:09 +0000

    some changes for practice purpose

---
+++ class-02/array.js
@@ -0,0 +1,20 @@
+const arr = [1, 2, 3, 4, 5];
+const frtArr = ["Banana", "Apple", "Mango"];
+const newArr = new Array(5, 4, 3, 2, 1);
+
+// console.log(arr[1]);
+console.log(arr);
+
+// ******************** ARRAY METHODS ******************** \\
+
+arr.push(6); // Value Add Last
+arr.pop(); // Last Value Remove
+arr.unshift(0); // Value Add Start
+arr.shift(); // Fiest Value Remove
+
+console.log(arr.includes(9)); // True/False
+console.log(arr.indexOf(3)); // Array K Value Ki Index Deta Hai
+
+
+
+console.log(arr);
