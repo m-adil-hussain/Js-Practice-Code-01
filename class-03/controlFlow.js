9e4e7555c9e3444a900cd23056dcd4302b4298de
Muhammad Adil Hussain <146125640+madilhussain@users.noreply.github.com>
2024-09-23 17:54:19 +0000

    some changes for practice purpose

--- class-03/controlFlow.js
+++ class-03/controlFlow.js
@@ -63,3 +63,68 @@ switch (month) {
         console.log("April");
         break;
 }
+
+
+const userEmail = "adil@gmail.com"; // True
+const userEmail = ""; // False
+const userEmail = []; // True
+
+if (userEmail) {
+    console.log("True!");
+} else {
+    console.log("False!");
+}
+
+// ******************** FALSY ******************** \\
+/*
+    false
+    0
+    -0
+    BigInt 0n
+    ""
+    null
+    undefined
+    NaN
+*/
+// ******************** TRUTHY ******************** \\
+/*
+    "0"
+    "false"
+    " "
+    []
+    {}
+    function() {}
+*/
+
+const emptyArr = [];
+
+if (emptyArr.length === 0) {
+    console.log("True!");
+}
+
+const emptyObj = {};
+
+if (Object.keys(emptyObj).length === 0) {
+    console.log("True!");
+}
+
+// ******************** NULLISH COALESCING OPERATOR (??) ******************** \\
+
+/*
+    * Ye "Null" Aur "Undefined" Operator Pr Kam Krta Hai.
+    * "Null" Aur "Undefined" Sai Bachne K Liye Hm "Nullish" Operator Ka Use Krte Hai.
+*/
+
+let val1;
+val1 = 5 ?? 10; // 5
+val1 = null ?? 10; // 10
+val1 = null ?? undefined ?? 20; // 20
+console.log(val1);
+
+// ******************** TERNARY OPERATOR ******************** \\
+
+condition ? true : false;
+
+const teaPrice = 120;
+
+teaPrice <= 150 ? "True!" : "False!";
