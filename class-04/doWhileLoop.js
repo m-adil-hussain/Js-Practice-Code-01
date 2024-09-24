568a4a38e59ad48bcb182fe609936ca19d71111e
Muhammad Adil Hussain <146125640+madilhussain@users.noreply.github.com>
2024-09-24 17:21:17 +0000

    some changes for practice purpose

---
+++ class-04/doWhileLoop.js
@@ -0,0 +1,16 @@
+// ******************** DO WHILE LOOP ******************** \\
+
+let index = 0;
+do {
+    console.log(index);
+    index++
+} while (index < 10);
+
+
+let arr = ["Apple", "Banana", "Mango"];
+let arrNum = 0;
+
+do {
+    console.log(arr[arrNum]);
+    arrNum++;
+} while (arrNum < arr.length);
---
+++ class-04/whileLoop.js
@@ -0,0 +1,17 @@
+// ******************** WHILE LOOP ******************** \\
+
+let index = 0;
+
+while (index < 10) {
+    console.log(index);
+    index++;
+}
+
+
+let arr = ["Apple", "Banana", "Mango"];
+let arrNum = 0;
+
+while (arrNum < arr.length) {
+    console.log(arr[arrNum]);
+    arrNum++;
+}
