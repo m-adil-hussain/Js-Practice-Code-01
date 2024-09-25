ca664c3bc405046b52a2af3ad48d0ebc8ff7e634
Muhammad Adil Hussain <146125640+madilhussain@users.noreply.github.com>
2024-09-25 17:36:52 +0000

    some changes for practice purpose

---
+++ class-04/forEach.js
@@ -0,0 +1,38 @@
+const coding = ["JavaScript", "Python", "PHP", "C++", "C#"];
+
+coding.forEach(function (val) {
+    console.log(val);
+});
+
+coding.forEach((val) => {
+    console.log(val);
+});
+
+function printMe(val) {
+    console.log(val);
+}
+
+coding.forEach(printMe); // Function Ka Refrence Dena Hai Execute❌ Nahi Krna.
+
+coding.forEach((item, index, arr) => {
+    console.log(item, index, arr);
+});
+
+const arrObj = [
+    {
+        name: "Adil Hussain",
+        fatherName: "Ghulam Hussain"
+    },
+    {
+        name: "Muhammad Ahmed",
+        fatherName: "Iqbal"
+    },
+    {
+        name: "Muhammad Awais",
+        fatherName: "Hanif"
+    }
+]
+
+arrObj.forEach((val) => {
+    console.log(val.name, val.fatherName);
+});
