83bbe9b02a1204ca2405eefe1873bf983ac4fd3c
Muhammad Adil Hussain <146125640+madilhussain@users.noreply.github.com>
2024-09-29 14:45:46 +0000

    some changes for practice purpose

---
+++ class-05/DOMClass-01/script.js
@@ -0,0 +1,5 @@
+const title = document.getElementById("title");
+
+title.innerHTML; // 'Document Object Model | Chai aur Code <b>JavaScript</b>'
+title.innerText; // 'Document Object Model | Chai aur Code'
+title.textContent; // 'Document Object Model | Chai aur Code JavaScript'
