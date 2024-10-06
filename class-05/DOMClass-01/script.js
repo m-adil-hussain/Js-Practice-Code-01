c521a63df7b41995dcce53c493d3864047c19ce0
Muhammad Adil Hussain <146125640+madilhussain@users.noreply.github.com>
2024-10-06 09:23:38 +0000

    some changes for practice purpose

--- class-05/DOMClass-01/script.js
+++ class-05/DOMClass-01/script.js
@@ -3,3 +3,18 @@ const title = document.getElementById("title");
 title.innerHTML; // 'Document Object Model | Chai aur Code <b>JavaScript</b>'
 title.innerText; // 'Document Object Model | Chai aur Code'
 title.textContent; // 'Document Object Model | Chai aur Code JavaScript'
+
+
+document.getElementById("title"); // <h1 id="title" class="heading">Document Object Model | Chai aur Code <b>JavaScript</b></h1>
+document.getElementsByClassName("heading"); // Return HTMLCollection | Ismai Hm ForEach Loop Wagera Nahi Laga Sakte.
+document.querySelector("li"); // <li>one</li>
+document.querySelectorAll(".list-item"); // Retun NodeList | Ismai Hm ForEach Laga Sakte Hai Ye Arry Return Nahi Krta Laki Array Jesa Lagta Hai.
+
+/*
+    // NODELIST/HTMLCOLLECTION TO ARRAY
+        * NodeList Ya HTMLCollection Ko Array Me Convert Krne K liye:
+    Array.from(variable);
+*/
+
+const convertIntoArray = document.querySelectorAll(".list-item");
+const myConvertArray = Array.from(convertIntoArray);
