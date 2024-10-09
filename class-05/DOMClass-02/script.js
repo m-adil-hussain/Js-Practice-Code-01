58b7cd00c79d1916cd142df228811dbbbce6d162
Muhammad Adil Hussain <146125640+madilhussain@users.noreply.github.com>
2024-10-09 17:35:51 +0000

    some changes for practice purpose

--- class-05/DOMClass-02/script.js
+++ class-05/DOMClass-02/script.js
@@ -9,15 +9,14 @@ for (let i = 0; i < parent.children.length; i++) {
     const element = parent.children[i];
     console.log(element);
 }
+
+console.log(parent.firstElementChild); // First Element Child Return Krta Hai.
+console.log(parent.lastElementChild); // Last Element Child Return Krta Hai.
 */
-
-console.log(parent.firstElementChild);
-console.log(parent.lastElementChild);
-
 const childElem = document.querySelector(".day");
-console.log(childElem);
-console.log(childElem.parentElement);
-console.log(childElem.nextElementSibling);
+// console.log(childElem); // Return 👉 div.day
+// console.log(childElem.parentElement); // Return 👉 div.parent
+// console.log(childElem.nextElementSibling); // Return 👉 div.day
 
-console.log("Node:", parent.childNodes);
-
+console.log("Node:", parent.childNodes); // Return 👉 NodeList(15)
+// ISMY LINE BREAK AUR SPACE BHI INCLUDE HAI \\
