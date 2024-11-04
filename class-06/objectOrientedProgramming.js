cac8700d2506e25a50a6edc466243aa3a7fb8289
Muhammad Adil Hussain <146125640+madilhussain@users.noreply.github.com>
2024-11-04 14:42:42 +0000

    some changes for practice purpose

---
+++ class-06/objectOrientedProgramming.js
@@ -0,0 +1,23 @@
+const user = {
+    username: 'Adil Hussain',
+    email: 'adil@google.com',
+    password: '876543210',
+    getUserID: () => {
+        console.log('Adil Hussain (56423)');
+    }
+}
+
+console.log(user.username);
+console.log(user.getUserID());
+
+const userOne = (username, email, password) => {
+    this.username = username;
+    this.email = email;
+    this.password = password;
+    return this;
+}
+
+const user1 = new userOne('Adil Hussain', 'adil@gmail.com', '223344');
+const user2 = new userOne('Ahmed Raza', 'ahmed@gmail.com', '556677');
+console.log(user1);
+console.log(user2);
